58f55b1a58a24e251cee56ebd7e8a0cb7d2b1edb
SULEYMAN <vanatiev321@icloud.com>
2019-11-05 16:03:23 +0300

    HomePage

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -3,16 +3,17 @@ import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 
 import FightApp from "./FightComponents/FightApp";
 import PvEBoard from "./FightComponents/PvEContant/PvEBoard";
-import HomePage from "./Frontend/HomePage";
+import Faq from "./Frontend/Faq";
 import Skills from "./Frontend/Skills";
 
 class App extends React.Component {
   render() {
     return (
       <Router>
-        <div>
+        <div className="full-screen">
           
-          <Route exact path="/" component={HomePage} />
+          <Route exact path="/" component={Faq} />
+          {/* <Route exact path="/login" component={Login} /> */}
           <Route exact path="/skill" component={Skills} />
           <Route exact path="/figth" component={FightApp} />
           <Route exact path="/figth/pve" component={PvEBoard} />
