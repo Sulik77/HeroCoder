a48f48eba2816ef5267775743dd7e75395127066
Fucha97 <egor.fucha@gmail.com>
2019-11-06 20:13:27 +0300

    bcrypt

--- back/routes/index.js
+++ back/routes/index.js
@@ -4,7 +4,7 @@ const { sessionChecker } = require("../middleware/auth");
 const User = require("../models/users");
 const Skills = require("../models/skill");
 const TestJS = require("../models/questionJS");
-
+const bcrypt = require('bcrypt');
 const router = express.Router();
 
 router.get("/", sessionChecker, (req, res) => {
@@ -27,29 +27,28 @@ router.route("/login")
     const email = req.body.email;
     const password = req.body.password;
     const user = await User.findOne({ email });
-
+    const check = await bcrypt.compare(password, user.password);
     if (!user) {
-      res.json({ status: 1, error: "Введите email и password" });
-    } else if (user.password !== password) {
+      res.json({ status: 1, error: "Такой пользователь не найден" });
+    } else if (!check) {
       const massege = "Не правильный password или email";
       res.json({ status: 1, error: massege });
     } else {
       req.session.user = user;
-      res.json({ username: user.username, email: user.email, hero:user.hero });
+      res.json({ username: user.username, hero:user.hero });
     }
   });
-
-
 
 router.route("/signup")
   .post(async (req, res) => {
     let newUser = await User.findOne({ email: req.body.email });
-
     if (!newUser) {
+     const pass = await bcrypt.hash(req.body.password, 12);
+      console.log("pass",pass);
       const user = new User({
         username: req.body.username,
         email: req.body.email,
-        password: req.body.password,
+        password: pass,
         hero: {
           gold: 5,
           health: 300,
@@ -61,7 +60,6 @@ router.route("/signup")
       res.json(
         {
           username: user.username,
-          email: user.email,
           hero: { gold: user.hero.gold, health: user.hero.health, damage: user.hero.damage }
         });
     } else {
@@ -69,5 +67,7 @@ router.route("/signup")
       res.json({ status: 1, error: message });
     }
   });
+
+
 
 module.exports = router;
--- client/src/redux/reducer.js
+++ client/src/redux/reducer.js
@@ -1,10 +1,11 @@
 import { START_FIGHT, LOGIN, WIN_FIGHT } from "./types";
 
 const initialState = {
-  user: { username: " ", email: " ",hero:{}, avatar:
+  user: { username: " ",hero:{}, avatar:
   "https://media.hearthpwn.com/avatars/297/167/636023914413148543.png", },
   loggedin: false,
 };
+
 
 export default function(state = initialState, action) {
   switch (action.type) {
