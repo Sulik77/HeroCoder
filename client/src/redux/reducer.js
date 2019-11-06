4adea4c1a0435064b73f7e8daed54865814acd2d
Fucha97 <egor.fucha@gmail.com>
2019-11-06 18:49:21 +0300

    v 1.0

--- client/src/redux/reducer.js
+++ client/src/redux/reducer.js
@@ -1,21 +1,9 @@
 import { START_FIGHT, LOGIN, WIN_FIGHT } from "./types";
 
 const initialState = {
-  user: { username: " ", email: " " },
+  user: { username: " ", email: " ",hero:{}, avatar:
+  "https://media.hearthpwn.com/avatars/297/167/636023914413148543.png", },
   loggedin: false,
-  player: {
-    type: "player",
-    name: "player",
-    avatar:
-      "https://media.hearthpwn.com/avatars/297/167/636023914413148543.png",
-    percs: [],
-    stats: {
-      lvl: 1,
-      health: 300,
-      damage: 10
-    },
-    gold: 0
-  }
 };
 
 export default function(state = initialState, action) {
