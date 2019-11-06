2f485efa8fb087f12df2a4507e93d135d1117940
Fucha97 <egor.fucha@gmail.com>
2019-11-06 21:08:57 +0300

    Egor finish day 06.11

--- back/routes/index.js
+++ back/routes/index.js
@@ -1,15 +1,12 @@
 const randomArr = require("../functions/randomArr");
 const express = require("express");
-const { sessionChecker } = require("../middleware/auth");
 const User = require("../models/users");
 const Skills = require("../models/skill");
 const TestJS = require("../models/questionJS");
 const bcrypt = require('bcrypt');
 const router = express.Router();
 
-router.get("/", sessionChecker, (req, res) => {
 
-});
 
 router.get("/api/test", async (req, res) => {
   const questions = await TestJS.find();
@@ -27,15 +24,21 @@ router.route("/login")
     const email = req.body.email;
     const password = req.body.password;
     const user = await User.findOne({ email });
-    const check = await bcrypt.compare(password, user.password);
+
     if (!user) {
+      console.log("error user");
       res.json({ status: 1, error: "Такой пользователь не найден" });
-    } else if (!check) {
-      const massege = "Не правильный password или email";
-      res.json({ status: 1, error: massege });
-    } else {
-      req.session.user = user;
-      res.json({ username: user.username, hero:user.hero });
+    }
+    else {
+      const check = await bcrypt.compare(password, user.password);
+      if (!check) {
+        const message = "Не правильный password или email";
+        res.json({ status: 1, error: message });
+      } else {
+        console.log("nashel");
+        req.session.user = user;
+        res.json({ username: user.username, hero: user.hero });
+      }
     }
   });
 
@@ -43,8 +46,7 @@ router.route("/signup")
   .post(async (req, res) => {
     let newUser = await User.findOne({ email: req.body.email });
     if (!newUser) {
-     const pass = await bcrypt.hash(req.body.password, 12);
-      console.log("pass",pass);
+      const pass = await bcrypt.hash(req.body.password, 12);
       const user = new User({
         username: req.body.username,
         email: req.body.email,
@@ -68,6 +70,9 @@ router.route("/signup")
     }
   });
 
-
+router.get("/api/check-sesion", async (req, res) => {
+  const user = req.session.user;
+  res.json(user);
+});
 
 module.exports = router;
